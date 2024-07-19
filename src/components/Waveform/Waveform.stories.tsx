d9dfe39e3ad313282acd03789d090f60860b253d
Ahhyun <ahhyun@pozalabs.com>
2024-07-19 14:48:59 +0900

    u: waveform 스토리 업데이트

--- src/components/Waveform/Waveform.stories.tsx
+++ src/components/Waveform/Waveform.stories.tsx
@@ -9,8 +9,18 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Primary: Story = {
+const src =
+  'https://files.freemusicarchive.org/storage-freemusicarchive-org/tracks/FJ0jWTSfcbi51kNU7wxl6fJNMyjFEpRse1IkpBXx.mp3';
+
+export const Canvas: Story = {
   args: {
-    className: 'bg-black text-white',
+    src,
+    type: 'canvas',
+  },
+};
+export const Svg: Story = {
+  args: {
+    src,
+    type: 'svg',
   },
 };
