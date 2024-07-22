240d1a585e95830149cfba0b251d002e61775edc
Ahhyun <ahhyun@pozalabs.com>
2024-07-22 10:39:41 +0900

    u: blob polyfill 설정 추가

--- vitest.setup.js
+++ vitest.setup.js
@@ -1,10 +1,14 @@
 import { afterAll, afterEach, beforeAll } from 'vitest';
 import { setupServer } from 'msw/node';
+import { Blob } from 'blob-polyfill';
 
 import handlers from './src/mocks/handlers';
 
 const server = setupServer(...handlers);
 
-beforeAll(() => server.listen({ onUnhandledRequest: 'error' }));
+beforeAll(() => {
+  global.Blob = Blob;
+  server.listen({ onUnhandledRequest: 'error' });
+});
 afterAll(() => server.close());
 afterEach(() => server.resetHandlers());
