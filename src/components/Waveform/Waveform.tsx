2f4c0e472dafc65261e57686fde2248747d4a884
Ahhyun <ahhyun@pozalabs.com>
2024-07-26 08:29:17 +0900

    u: Waveform ref 타입 지정

--- src/components/Waveform/Waveform.tsx
+++ src/components/Waveform/Waveform.tsx
@@ -1,16 +1,23 @@
-import { forwardRef, useEffect, useImperativeHandle, useRef } from 'react';
-import useWaveform, {
-  type UseWaveformParams,
-} from '../../hooks/waveform/useWaveform';
+import { ForwardedRef, forwardRef, useEffect, useImperativeHandle, useRef } from 'react';
+import useWaveform, { type UseWaveformParams } from '../../hooks/waveform/useWaveform';
+
+export interface WaveformHandles {
+  isPlaying: boolean;
+  currentTime: number;
+  duration: number;
+  play: () => void;
+  pause: () => void;
+  changeCurrentTime: (currentTime: number) => void;
+}
 
 /**
  * useWaveform 훅을 사용하여 렌더링하는 Waveform 컴포넌트입니다.
  *
  * 전달하는 ref를 통해 오디오에 대한 정보를 얻거나 제어할 수 있습니다.
  *
- * (주의: 전달하는 오디오 상태는 실시간으로 바뀌지 않습니다. 필요할 때 접근해서 사용해주세요.)
+ * (주의: 전달하는 오디오 상태는 ref를 사용하기 때문에 실시간으로 바뀌지 않습니다. 필요할 때 접근해서 사용해주세요.)
  * ```ts
- * ref.current = {
+ * interface WaveformHandles {
  *    isPlaying: boolean;
  *    currentTime: number;
  *    duration: number;
@@ -20,16 +27,9 @@ import useWaveform, {
  * }
  * ```
  */
-const Waveform = forwardRef((props: UseWaveformParams, ref) => {
-  const {
-    waveform,
-    isPlaying,
-    currentTime,
-    duration,
-    play,
-    pause,
-    changeCurrentTime,
-  } = useWaveform(props);
+const Waveform = forwardRef((props: UseWaveformParams, ref: ForwardedRef<WaveformHandles>) => {
+  const { waveform, isPlaying, currentTime, duration, play, pause, changeCurrentTime } =
+    useWaveform(props);
 
   const containerRef = useRef<HTMLDivElement>(null);
 
@@ -44,10 +44,7 @@ const Waveform = forwardRef((props: UseWaveformParams, ref) => {
     const waveformElement = waveform as Element;
     waveformElement.setAttribute('role', 'waveform');
 
-    containerRef.current.replaceChild(
-      waveformElement,
-      containerRef.current.firstChild!,
-    );
+    containerRef.current.replaceChild(waveformElement, containerRef.current.firstChild!);
   }, [waveform]);
 
   useImperativeHandle(
