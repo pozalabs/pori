f8e4302e249ceb59b544ab6a97c97d46bf6b2f11
Ahhyun <ahhyun@pozalabs.com>
2024-07-29 11:36:17 +0900

    u: WaveformHandles export 구문 수정

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 import '../styles/global.css';
 
-export { default as Waveform, WaveformHandles } from './Waveform/Waveform';
+export { default as Waveform } from './Waveform/Waveform';
+export type { WaveformHandles } from './Waveform/Waveform';
