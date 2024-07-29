ab1ec2397ef70405e701b387ac4593a374927cdd
Ahhyun <ahhyun@pozalabs.com>
2024-07-29 13:13:02 +0900

    u: Konva waveform 스토리 추가

--- src/components/Waveform/Waveform.stories.tsx
+++ src/components/Waveform/Waveform.stories.tsx
@@ -1,7 +1,10 @@
-import { useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
+import { KonvaEventObject } from 'konva/lib/Node';
+import { Image, Layer, Stage } from 'react-konva';
+
 import Waveform, { WaveformHandles } from './Waveform';
-import { UseWaveformParams } from '../../hooks/waveform/useWaveform';
+import useWaveform, { UseWaveformParams } from '../../hooks/waveform/useWaveform';
 import { WAVEFORM_DEFAULT_VALUE } from '../../hooks/waveform/_constants';
 
 const WaveformWithControlButton = (props: UseWaveformParams) => {
@@ -44,4 +47,84 @@ export const Canvas: Story = {
 };
 export const Svg: Story = {
   args: { ...WAVEFORM_DEFAULT_VALUE, type: 'svg', src },
+};
+export const Konva: Story = {
+  args: { ...WAVEFORM_DEFAULT_VALUE, type: 'canvas', src },
+  render: (props: UseWaveformParams) => {
+    const { waveform, play, pause, changeCurrentTime, isPlaying, duration } = useWaveform(props);
+    const isDraggingRef = useRef(false);
+    const ref = useRef<any>();
+
+    const updateCurrentTime = (e: KonvaEventObject<MouseEvent>): void => {
+      const rect = e.target.getClientRect();
+      const clickPosition = e.target.getRelativePointerPosition();
+
+      if (!clickPosition) return;
+
+      const targetWidth = rect.width;
+      const percent = (clickPosition.x / targetWidth) * 100;
+      const newCurrentTime = (percent * duration) / 100;
+
+      changeCurrentTime(newCurrentTime);
+    };
+
+    useEffect(() => {
+      let frameId = 0;
+
+      const drawLayer = (): void => {
+        if (!isPlaying) return;
+
+        ref.current.batchDraw();
+        frameId = requestAnimationFrame(drawLayer);
+      };
+      frameId = requestAnimationFrame(drawLayer);
+
+      return () => cancelAnimationFrame(frameId);
+    }, []);
+
+    const onElementMouseDown = (): void => {
+      isDraggingRef.current = true;
+    };
+    const onElementMouseUp = (): void => {
+      isDraggingRef.current = false;
+    };
+    const onElementMouseMove = (e: KonvaEventObject<MouseEvent>): void => {
+      if (!isDraggingRef.current) return;
+
+      updateCurrentTime(e);
+    };
+
+    return (
+      <div className="flex flex-col gap-[1rem] w-fit">
+        <Stage width={props.width} height={props.height! * 2}>
+          <Layer ref={ref}>
+            <Image
+              image={waveform}
+              alt="waveform"
+              onClick={updateCurrentTime}
+              onMouseDown={onElementMouseDown}
+              onMouseUp={onElementMouseUp}
+              onMouseLeave={onElementMouseUp}
+              onMouseMove={onElementMouseMove}
+              y={50}
+            />
+          </Layer>
+        </Stage>
+        <div className="flex justify-between px-[12rem]">
+          <button
+            className="bg-blue-50 hover:bg-blue-100 py-[0.5rem] px-[1rem] border border-blue-100 rounded-[8px] transition duration-100 ease-in-out"
+            onClick={play}
+          >
+            재생
+          </button>
+          <button
+            className="bg-blue-50 hover:bg-blue-100 py-[0.5rem] px-[1rem] border border-blue-100 rounded-[8px] transition duration-100 ease-in-out"
+            onClick={pause}
+          >
+            일시정지
+          </button>
+        </div>
+      </div>
+    );
+  },
 };
