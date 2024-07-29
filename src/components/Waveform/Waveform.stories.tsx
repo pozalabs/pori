301efbcc0073b084e89ad3a968a40b696287da8c
Ahhyun <ahhyun@pozalabs.com>
2024-07-29 10:04:26 +0900

    u: waveform story 설정

--- src/components/Waveform/Waveform.stories.tsx
+++ src/components/Waveform/Waveform.stories.tsx
@@ -1,9 +1,36 @@
+import { useRef } from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
-import Waveform from './Waveform';
+import Waveform, { WaveformHandles } from './Waveform';
+import { UseWaveformParams } from '../../hooks/waveform/useWaveform';
+import { WAVEFORM_DEFAULT_VALUE } from '../../hooks/waveform/_constants';
+
+const WaveformWithControlButton = (props: UseWaveformParams) => {
+  const ref = useRef<WaveformHandles>(null);
+
+  return (
+    <div className="flex flex-col gap-[1rem] w-fit">
+      <Waveform ref={ref} {...props} />
+      <div className="flex justify-between px-[12rem]">
+        <button
+          className="bg-blue-50 hover:bg-blue-100 py-[0.5rem] px-[1rem] border border-blue-100 rounded-[8px] transition duration-100 ease-in-out"
+          onClick={() => ref.current?.play()}
+        >
+          재생
+        </button>
+        <button
+          className="bg-blue-50 hover:bg-blue-100 py-[0.5rem] px-[1rem] border border-blue-100 rounded-[8px] transition duration-100 ease-in-out"
+          onClick={() => ref.current?.pause()}
+        >
+          일시정지
+        </button>
+      </div>
+    </div>
+  );
+};
 
 const meta = {
   title: 'Waveform',
-  component: Waveform,
+  component: WaveformWithControlButton,
 } satisfies Meta<typeof Waveform>;
 
 export default meta;
@@ -13,14 +40,8 @@ const src =
   'https://files.freemusicarchive.org/storage-freemusicarchive-org/tracks/FJ0jWTSfcbi51kNU7wxl6fJNMyjFEpRse1IkpBXx.mp3';
 
 export const Canvas: Story = {
-  args: {
-    src,
-    type: 'canvas',
-  },
+  args: { ...WAVEFORM_DEFAULT_VALUE, type: 'canvas', src },
 };
 export const Svg: Story = {
-  args: {
-    src,
-    type: 'svg',
-  },
+  args: { ...WAVEFORM_DEFAULT_VALUE, type: 'svg', src },
 };
