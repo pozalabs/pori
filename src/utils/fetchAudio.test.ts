8aad598c648a549a0549f7f6e1ef797bc1e4bdf6
Ahhyun <ahhyun@pozalabs.com>
2024-08-06 11:07:13 +0900

    u: fetchAudio test 파일 retry 횟수 1로 수정

--- src/utils/fetchAudio.test.ts
+++ src/utils/fetchAudio.test.ts
@@ -13,19 +13,19 @@ describe('fetchAudio 청크 다운로드 동작 테스트', () => {
   });
 
   it('30MB인 파일에 대해서 1MB 단위로 청크 다운로드를 진행한다.', async () => {
-    await fetchAudio({ src: FILE_SRC['30'] });
+    await fetchAudio({ src: FILE_SRC['30'], retry: 1 });
 
     expect(fetch).toBeCalledTimes(30 / 1 + 1);
   });
 
   it('75MB인 파일에 대해서 5MB 단위로 청크 다운로드를 진행한다.', async () => {
-    await fetchAudio({ src: FILE_SRC['75'] });
+    await fetchAudio({ src: FILE_SRC['75'], retry: 1 });
 
     expect(fetch).toBeCalledTimes(75 / 5 + 1);
   });
 
   it('100MB인 파일에 대해서 10MB 단위로 청크 다운로드를 진행한다.', async () => {
-    await fetchAudio({ src: FILE_SRC['100'] });
+    await fetchAudio({ src: FILE_SRC['100'], retry: 1 });
 
     expect(fetch).toBeCalledTimes(100 / 10 + 1);
   });
