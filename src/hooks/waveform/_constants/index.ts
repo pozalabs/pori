e6a2cd8be63b5910d99fdbc1a40ffa764e680e3e
Ahhyun <ahhyun@pozalabs.com>
2024-08-06 14:49:24 +0900

    u: playhead default width를 1로 수정

--- src/hooks/waveform/_constants/index.ts
+++ src/hooks/waveform/_constants/index.ts
@@ -4,7 +4,7 @@ export const WAVEFORM_DEFAULT_VALUE = {
   peakLength: 1024,
   width: 1000,
   height: 100,
-  playheadWidth: 0.25,
+  playheadWidth: 1,
   waveColor: 'black',
   progressColor: '#0873ff',
   bgColor: 'transparent',
