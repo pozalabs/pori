761545ffbb651551df76560f463b169513225496
Ahhyun <ahhyun@pozalabs.com>
2024-08-07 11:13:36 +0900

    u: Waveform 컴포넌트 스토리북 수정

--- src/components/Waveform/Waveform.stories.tsx
+++ src/components/Waveform/Waveform.stories.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useMemo, useRef } from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
 import { KonvaEventObject } from 'konva/lib/Node';
 import { Image, Layer, Stage } from 'react-konva';
@@ -6,6 +6,7 @@ import { Image, Layer, Stage } from 'react-konva';
 import Waveform, { WaveformHandles } from './Waveform';
 import useWaveform, { UseWaveformParams } from '../../hooks/waveform/useWaveform';
 import { WAVEFORM_DEFAULT_VALUE } from '../../hooks/waveform/_constants';
+import formatTime from '../../hooks/waveform/_utils/formatTime';
 
 const WaveformWithControlButton = (props: UseWaveformParams) => {
   const ref = useRef<WaveformHandles>(null);
@@ -39,8 +40,7 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-const src =
-  'https://files.freemusicarchive.org/storage-freemusicarchive-org/tracks/FJ0jWTSfcbi51kNU7wxl6fJNMyjFEpRse1IkpBXx.mp3';
+const src = 'https://cdn.pixabay.com/audio/2023/06/12/audio_23ef6b7464.mp3';
 
 export const Canvas: Story = {
   args: { ...WAVEFORM_DEFAULT_VALUE, type: 'canvas', src },
@@ -49,11 +49,35 @@ export const Svg: Story = {
   args: { ...WAVEFORM_DEFAULT_VALUE, type: 'svg', src },
 };
 export const Konva: Story = {
-  args: { ...WAVEFORM_DEFAULT_VALUE, type: 'canvas', src },
+  args: {
+    ...WAVEFORM_DEFAULT_VALUE,
+    type: 'canvas',
+    width: 400,
+    height: 50,
+    peakLength: 100,
+    waveColor: '#E0E1E6',
+    progressColor: '#BBF500',
+    playheadBgColor: '#FF5924',
+    bgColor: '#8D8D86',
+    src,
+  },
   render: (props: UseWaveformParams) => {
-    const { waveform, play, pause, changeCurrentTime, duration } = useWaveform(props);
-    const isDraggingRef = useRef(false);
+    const {
+      waveform,
+      play,
+      pause,
+      changeCurrentTime,
+      showPlayhead,
+      hidePlayhead,
+      duration,
+      currentTime,
+    } = useWaveform(props);
     const ref = useRef<any>();
+    const dpr = useMemo(() => window.devicePixelRatio || 1, []);
+
+    const handleMouseMove = (e: KonvaEventObject<MouseEvent>) => {
+      showPlayhead(e.evt);
+    };
 
     const updateCurrentTime = (e: KonvaEventObject<MouseEvent>): void => {
       const rect = e.target.getClientRect();
@@ -62,7 +86,8 @@ export const Konva: Story = {
       if (!clickPosition) return;
 
       const targetWidth = rect.width;
-      const percent = (clickPosition.x / targetWidth) * 100;
+      const clickX = clickPosition.x / dpr;
+      const percent = (clickX / targetWidth) * 100;
       const newCurrentTime = (percent * duration) / 100;
 
       changeCurrentTime(newCurrentTime);
@@ -80,47 +105,42 @@ export const Konva: Story = {
       return () => cancelAnimationFrame(frameId);
     }, []);
 
-    const onElementMouseDown = (): void => {
-      isDraggingRef.current = true;
-    };
-    const onElementMouseUp = (): void => {
-      isDraggingRef.current = false;
-    };
-    const onElementMouseMove = (e: KonvaEventObject<MouseEvent>): void => {
-      if (!isDraggingRef.current) return;
-
-      updateCurrentTime(e);
-    };
-
     return (
-      <div className="flex flex-col gap-[1rem] w-fit">
-        <Stage width={props.width} height={props.height! * 2}>
+      <div className="flex flex-col items-center gap-[2rem] p-[1rem] bg-[#8d8d86] rounded-lg">
+        <span className="text-white self-start">audio_23ef6b7464.mp3</span>
+        <Stage width={props.width} height={props.height}>
           <Layer ref={ref}>
             <Image
               image={waveform}
               alt="waveform"
               onClick={updateCurrentTime}
-              onMouseDown={onElementMouseDown}
-              onMouseUp={onElementMouseUp}
-              onMouseLeave={onElementMouseUp}
-              onMouseMove={onElementMouseMove}
-              y={50}
+              onMouseMove={handleMouseMove}
+              onMouseOut={hidePlayhead}
+              scale={{ x: 1 / dpr, y: 1 / dpr }}
             />
           </Layer>
         </Stage>
-        <div className="flex justify-between px-[12rem]">
-          <button
-            className="bg-blue-50 hover:bg-blue-100 py-[0.5rem] px-[1rem] border border-blue-100 rounded-[8px] transition duration-100 ease-in-out"
-            onClick={play}
-          >
-            재생
-          </button>
-          <button
-            className="bg-blue-50 hover:bg-blue-100 py-[0.5rem] px-[1rem] border border-blue-100 rounded-[8px] transition duration-100 ease-in-out"
-            onClick={pause}
-          >
-            일시정지
-          </button>
+        <div className="flex justify-between items-center w-full">
+          <span className="text-white tracking-widest w-[40px] text-[14px] font-light">
+            {formatTime(currentTime)}
+          </span>
+          <div className="flex gap-[8px]">
+            <button
+              className="bg-[#E0E1E6] hover:bg-[#CDCED6] py-[0.5rem] px-[1rem] rounded-[8px] transition duration-100 ease-in-out"
+              onClick={play}
+            >
+              재생
+            </button>
+            <button
+              className="bg-[#E0E1E6] hover:bg-[#CDCED6] py-[0.5rem] px-[1rem] rounded-[8px] transition duration-100 ease-in-out"
+              onClick={pause}
+            >
+              일시정지
+            </button>
+          </div>
+          <span className="text-white tracking-widest w-[40px] text-[14px] font-light">
+            {formatTime(duration)}
+          </span>
         </div>
       </div>
     );
