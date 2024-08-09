d84ccc085c164320cff08260860eaa399eb65893
Ahhyun <ahhyun@pozalabs.com>
2024-08-09 13:55:01 +0900

    u: gap이 0 미만일 때 에러가 발생하지 않도록 처리

--- src/hooks/waveform/_utils/getPeakLength.ts
+++ src/hooks/waveform/_utils/getPeakLength.ts
@@ -1,7 +1,9 @@
 import { BAR_WIDTH } from '../_constants';
 
 const getPeakLength = (width: number, gap: number): number => {
-  const peakLength = Math.floor(width / (gap + BAR_WIDTH));
+  const processedGap = gap >= 0 ? gap : 0;
+
+  const peakLength = Math.floor(width / (processedGap + BAR_WIDTH));
 
   return peakLength;
 };
