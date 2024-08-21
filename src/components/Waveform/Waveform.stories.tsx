650707188e6e846e2ba8ef551e679d33c5ab3f2e
Eunjeong <88300485+eunjeongy@users.noreply.github.com>
2024-08-21 13:38:49 +0900

    IP-5480: [pori] Konva Transformer 적용 (#23)
    * u: transformer 적용
    
    * u: 순서 변경
    
    * u: lint fix 적용

--- src/components/Waveform/Waveform.stories.tsx
+++ src/components/Waveform/Waveform.stories.tsx
@@ -1,9 +1,11 @@
 /* eslint-disable react-hooks/rules-of-hooks */
-import { useEffect, useMemo, useRef } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 
 import type { Meta, StoryObj } from '@storybook/react';
+import type { Group as TGroup } from 'konva/lib/Group';
 import type { KonvaEventObject } from 'konva/lib/Node';
-import { Image, Layer, Stage } from 'react-konva';
+import type { Transformer as TTransformer } from 'konva/lib/shapes/Transformer';
+import { Group, Image, Layer, Rect, Stage, Text, Transformer } from 'react-konva';
 
 import type { WaveformHandles } from './Waveform';
 import Waveform from './Waveform';
@@ -57,14 +59,26 @@ export const Konva: Story = {
     type: 'canvas',
     variant: 'bar',
     width: 400,
-    height: 50,
+    height: 60,
     gap: 2,
-    waveColor: '#E0E1E6',
-    progressColor: '#BBF500',
-    bgColor: '#8D8D86',
+    waveColor: '#8d8d8d',
+    progressColor: '#F24905',
+    hoveredColor: '#F98868',
+    bgColor: '#E1DCCF',
     src,
   },
   render: (props: UseWaveformParams) => {
+    const [audioItem, setAudioItem] = useState({
+      x: 100,
+      y: 100,
+      width: 440,
+      height: 148,
+      rotation: 0,
+    });
+    const AUDIO_PADDING_X = 12;
+    const AUDIO_PADDING_Y = 20;
+    const [waveformProps, setWaveformProps] = useState(props);
+
     const {
       waveform,
       play,
@@ -73,13 +87,54 @@ export const Konva: Story = {
       showHoveredWaveform,
       hideHoveredWaveform,
       duration,
-    } = useWaveform(props);
+    } = useWaveform(waveformProps);
+
+    const SCALE = audioItem.height / 148;
+    const MAX_WIDTH = audioItem.width - AUDIO_PADDING_X * 2 * SCALE;
+
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const ref = useRef<any>();
+    const trRef = useRef<TTransformer>(null);
+    const shapeRef = useRef<TGroup>(null);
     const dpr = useMemo(() => window.devicePixelRatio || 1, []);
+    const [selected, setSelected] = useState(false);
+
+    const onDragEnd = (): void => {
+      if (!shapeRef.current) return;
+      const node = shapeRef.current;
+      setAudioItem({ ...audioItem, x: node.x(), y: node.y() });
+    };
+
+    const handleTransformEnd = (): void => {
+      if (!shapeRef.current) return;
+      const node = shapeRef.current;
+      const scaleX = node.scaleX();
+      const scaleY = node.scaleY();
+
+      node.scaleX(1);
+      node.scaleY(1);
+
+      const width = node.width() * scaleX;
+      const height = node.height() * scaleY;
+
+      setAudioItem({ x: node.x(), y: node.y(), rotation: node.rotation(), width, height });
+
+      setWaveformProps({
+        ...waveformProps,
+        height: 60 * (height / 148),
+        width: width - AUDIO_PADDING_X * 2 * scaleX,
+      });
+    };
+
+    const onSelectNode = (e: KonvaEventObject<MouseEvent>) => {
+      if (e.target.name() === 'waveform') return;
+      setSelected(true);
+    };
 
     const handleMouseMove = (e: KonvaEventObject<MouseEvent>) => {
-      showHoveredWaveform(e.evt);
+      const pointerPositionX = e.target.getRelativePointerPosition()?.x;
+      const playheadPosition = pointerPositionX ? pointerPositionX / dpr : undefined;
+      showHoveredWaveform(e.evt, playheadPosition);
     };
 
     const updateCurrentTime = (e: KonvaEventObject<MouseEvent>): void => {
@@ -108,19 +163,75 @@ export const Konva: Story = {
       return () => cancelAnimationFrame(frameId);
     }, []);
 
+    useEffect(() => {
+      if (!shapeRef.current) return;
+      if (selected) {
+        // we need to attach transformer manually
+        trRef.current?.nodes([shapeRef.current]);
+        trRef.current?.getLayer()?.batchDraw();
+      }
+    }, [selected, waveform]);
+
     return (
-      <div className="flex flex-col items-center gap-8 rounded-lg bg-[#8d8d86] p-4">
-        <span className="self-start text-white">audio_23ef6b7464.mp3</span>
-        <Stage width={props.width} height={props.height}>
+      <div className="flex flex-col items-center gap-8 rounded-lg bg-[#f0f0f3] p-4">
+        <Stage width={window.innerWidth} height={300}>
           <Layer ref={ref}>
-            <Image
-              image={waveform}
-              alt="waveform"
-              onClick={updateCurrentTime}
-              onMouseMove={handleMouseMove}
-              onMouseOut={hideHoveredWaveform}
-              scale={{ x: 1 / dpr, y: 1 / dpr }}
-            />
+            <Group
+              x={audioItem.x}
+              y={audioItem.y}
+              width={audioItem.width}
+              height={audioItem.height}
+              rotation={audioItem.rotation}
+              draggable
+              ref={shapeRef}
+              onClick={onSelectNode}
+              onDragEnd={onDragEnd}
+              onTransformEnd={handleTransformEnd}
+            >
+              <Rect
+                fill="#E1DCCF"
+                width={audioItem.width}
+                height={audioItem.height}
+                x={0}
+                y={0}
+                cornerRadius={6}
+              />
+              <Text
+                x={AUDIO_PADDING_X * SCALE}
+                y={AUDIO_PADDING_Y * SCALE}
+                text="audio_23ef6b7464.mp3"
+                fontSize={12 * SCALE}
+                lineHeight={1.33}
+                width={MAX_WIDTH}
+                fill="#344217"
+                fontFamily="Inter"
+                wrap="none"
+                ellipsis={true}
+              />
+              <Image
+                name="waveform"
+                image={waveform}
+                alt="waveform"
+                onClick={updateCurrentTime}
+                x={AUDIO_PADDING_X * SCALE}
+                y={36 * SCALE}
+                onMouseMove={handleMouseMove}
+                onMouseOut={hideHoveredWaveform}
+                scale={{ x: 1 / dpr, y: 1 / dpr }}
+              />
+            </Group>
+            {selected && (
+              <Transformer
+                ref={trRef}
+                flipEnabled={false}
+                borderStrokeWidth={1.5}
+                borderStroke="#5F7F00"
+                enabledAnchors={['top-left', 'top-right', 'bottom-left', 'bottom-right']}
+                anchorStroke="#5F7F00"
+                anchorStrokeWidth={1}
+                rotateAnchorOffset={30}
+              />
+            )}
           </Layer>
         </Stage>
         <div className="flex w-full items-center justify-between">
