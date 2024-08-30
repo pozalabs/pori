02d757dd94c9c619164d315d13c9b7c1a7183bb0
Ahhyun <141609292+ahhyun-dev@users.noreply.github.com>
2024-08-30 10:41:54 +0900

    IP-5510: [pori] waveform에 OfflineAudioContext 적용 (#27)
    * u: useAudioData의 AudioContext를 OfflineAudioContext로 변경
    
    * u: OfflineAudioContext param 값을 객체로 수정
    
    * u: 오디오 컨텍스트 mocking 수정
    
    * u: OfflineAudioContext length 상수화 및 주석 작성

--- src/components/Waveform/Waveform.test.tsx
+++ src/components/Waveform/Waveform.test.tsx
@@ -1,5 +1,5 @@
 import { render } from '@testing-library/react';
-import { AudioContext } from 'standardized-audio-context-mock';
+import { OfflineAudioContext } from 'standardized-audio-context-mock';
 import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest';
 import 'vitest-canvas-mock';
 
@@ -7,12 +7,12 @@ import Waveform from './Waveform';
 import { FILE_SRC } from '../../mocks/constants';
 
 describe('Waveform 컴포넌트 렌더링 테스트', () => {
-  let windowAudioContext: typeof window.AudioContext;
+  let offlineAudioContext: typeof window.OfflineAudioContext;
 
   beforeEach(() => {
-    windowAudioContext = window.AudioContext;
+    offlineAudioContext = window.OfflineAudioContext;
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    window.AudioContext = AudioContext as any;
+    window.OfflineAudioContext = OfflineAudioContext as any;
     window.HTMLMediaElement.prototype.pause = vi.fn();
     window.OffscreenCanvas = vi.fn().mockImplementation((width: number, height: number) => {
       return {
@@ -31,7 +31,7 @@ describe('Waveform 컴포넌트 렌더링 테스트', () => {
   });
 
   afterEach(() => {
-    window.AudioContext = windowAudioContext;
+    window.OfflineAudioContext = offlineAudioContext;
     (window.HTMLMediaElement.prototype.pause as ReturnType<typeof vi.fn>).mockClear();
     (window.OffscreenCanvas as ReturnType<typeof vi.fn>).mockClear();
   });
