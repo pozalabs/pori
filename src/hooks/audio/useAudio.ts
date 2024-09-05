56c9a810ab5e0da8a3998345263a3bd9309645c5
Ahhyun <ahhyun@pozalabs.com>
2024-09-05 15:05:50 +0900

    u: useAudio 설명 주석 추가

--- src/hooks/audio/useAudio.ts
+++ src/hooks/audio/useAudio.ts
@@ -17,6 +17,44 @@ interface UseAudioReturns extends UseAudioStateReturns, UseControlAudioReturns {
   audioRef: MutableRefObject<HTMLAudioElement>;
 }
 
+/**
+ * 오디오를 제어할 수 있는 훅입니다. 현재는 오디오 재생 및 볼륨 관련하여 조작과 사용이 가능합니다.
+ * @param UseAudioParams
+ * ```
+ * interface UseAudioParams {
+ *    autoplay?: boolean;
+ *    maxProgressTime?: number;
+ *    maxProgressVolume?: number;
+ *    src?: string;
+ * }
+ * ```
+ * - `autoplay` : 오디오 자동 재생 여부 (default : false)
+ * - `maxProgressTime` : 현재 재생 시간을 progress로 환산했을 때의 max 값 (default : 100)
+ * - `maxProgressVolume` : 현재 볼륨의 max 값 (default : 1)
+ * - `src` : 오디오 source url
+ * @returns
+ * `UseAudioReturns`
+ * ```
+ * interface UseAudioReturns {
+ *    audioRef: MutableRefObject<HTMLAudioElement>;
+ *    currentSrc: string;
+ *    currentTime: number;
+ *    duration: number;
+ *    isPlaying: boolean;
+ *    progressTime: number;
+ *    volume: number;
+ *    changeCurrentSrc: (currentSrc: string) => void;
+ *    changeCurrentTime: (currentTime: number) => void;
+ *    changeProgressTime: (progress: number) => void;
+ *    changeVolume: (volume: number) => void;
+ *    play: () => void;
+ *    pause: () => void;
+ *    resetAudioTime: () => void;
+ *    toggleMuted: () => void;
+ *    togglePlayPause: (src?: string) => void;
+ * }
+ * ```
+ */
 const useAudio = ({
   autoplay = AUDIO_DEFAULT_VALUE.autoplay,
   maxProgressTime = AUDIO_DEFAULT_VALUE.maxProgressTime,
