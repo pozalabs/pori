43ad7d242661daad27ebf8564dbfb4bc743317c1
Ahhyun <ahhyun@pozalabs.com>
2024-09-23 15:33:41 +0900

    c: 간단한 Slider 스토리 추가

--- src/components/Slider/Slider.stories.tsx
+++ src/components/Slider/Slider.stories.tsx
@@ -1,29 +1,16 @@
-import { useState } from 'react';
+/* eslint-disable react-hooks/rules-of-hooks */
+import { useEffect, useRef, useState } from 'react';
 
 import type { Meta, StoryObj } from '@storybook/react';
 
 import { SLIDER_DEFAULT_VALUE } from './_constants';
 import Slider from './Slider';
-
-const DemoComponent = (params: Parameters<typeof Slider>[0]) => {
-  const [value, setValue] = useState(0);
-
-  const onValueChange = (value: number): void => {
-    setValue(value);
-  };
-
-  return (
-    <div>
-      <Slider value={value} onValueChange={onValueChange} onDrag={onValueChange} {...params} />
-      <span>{value}</span>
-    </div>
-  );
-};
+import { useAudio } from '../../hooks';
 
 const meta = {
   title: 'Slider',
-  component: DemoComponent,
-} satisfies Meta<typeof DemoComponent>;
+  component: Slider,
+} satisfies Meta<typeof Slider>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
@@ -33,5 +20,124 @@ export const Default: Story = {
     ...SLIDER_DEFAULT_VALUE,
     className: 'w-[400px] h-[20px] rounded-full',
     step: 5,
+  },
+  render: (props: Parameters<typeof Slider>[0]) => {
+    const [value, setValue] = useState(0);
+
+    const onValueChange = (value: number): void => {
+      setValue(value);
+    };
+
+    return (
+      <div className="relative">
+        <Slider value={value} onValueChange={onValueChange} onDrag={onValueChange} {...props} />
+        <span className="absolute" style={{ left: `${(value / props.max!) * 100 - 2}%` }}>
+          {value}
+        </span>
+      </div>
+    );
+  },
+};
+
+export const Player: Story = {
+  args: {
+    ...SLIDER_DEFAULT_VALUE,
+    className: 'w-[400px]',
+    step: 0.01,
+  },
+  render: (props: Parameters<typeof Slider>[0]) => {
+    const isDragging = useRef(false);
+    const [dragTime, setDragTime] = useState(0);
+
+    const {
+      currentTime,
+      duration,
+      isPlaying,
+      volume,
+      changeCurrentTime,
+      changeVolume,
+      shiftTimeBackward,
+      shiftTimeForward,
+      togglePlayPause,
+    } = useAudio({
+      src: 'https://cdn.pixabay.com/audio/2023/06/12/audio_23ef6b7464.mp3',
+      enabledKeyboardControl: true,
+    });
+
+    const onCurrentTimeSliderChange = (currentTime: number): void => {
+      changeCurrentTime(currentTime);
+    };
+
+    const onCurrentTimeSliderDrag = (currentTime: number): void => {
+      if (isDragging.current) {
+        setDragTime(currentTime);
+        return;
+      }
+      changeCurrentTime(currentTime);
+    };
+
+    const onVolumeSliderChange = (volume: number): void => {
+      changeVolume(volume);
+    };
+
+    useEffect(() => {
+      const onMouseUp = () => {
+        if (!isDragging.current) return;
+
+        isDragging.current = false;
+        changeCurrentTime(dragTime);
+      };
+
+      document.addEventListener('mouseup', onMouseUp);
+
+      return () => {
+        document.removeEventListener('mouseup', onMouseUp);
+      };
+    }, [changeCurrentTime, dragTime]);
+
+    return (
+      <div className="flex flex-col items-center gap-4">
+        <Slider
+          {...props}
+          max={duration}
+          value={isDragging.current ? dragTime : currentTime}
+          onValueChange={onCurrentTimeSliderChange}
+          onDrag={onCurrentTimeSliderDrag}
+          onDragStart={() => (isDragging.current = true)}
+        />
+        <div className="flex items-center justify-center gap-2">
+          <span className="text-[12px]">볼륨</span>
+          <Slider
+            min={0}
+            max={1}
+            step={0.1}
+            value={volume}
+            onValueChange={onVolumeSliderChange}
+            onDrag={onVolumeSliderChange}
+            className="h-2 w-[100px]"
+          />
+        </div>
+        <div className="flex justify-center gap-2">
+          <button
+            className="rounded-md border border-slate-300 bg-slate-50 px-3 py-1"
+            onClick={shiftTimeForward}
+          >
+            앞으로 10초 이동
+          </button>
+          <button
+            className="rounded-md border border-slate-300 bg-slate-50 px-3 py-1"
+            onClick={() => togglePlayPause()}
+          >
+            {isPlaying ? '일시정지' : '재생'}
+          </button>
+          <button
+            className="rounded-md border border-slate-300 bg-slate-50 px-3 py-1"
+            onClick={shiftTimeBackward}
+          >
+            뒤로 10초 이동
+          </button>
+        </div>
+      </div>
+    );
   },
 };
