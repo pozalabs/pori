9ca60d222fea85da53dc9cd101917231d6a32d19
Ahhyun <ahhyun@pozalabs.com>
2024-09-24 09:07:54 +0900

    u: tailwind border radius inherit extends 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: ['class'],
   theme: {
-    extend: {},
+    extend: {
+      borderRadius: {
+        inherit: 'inherit',
+      },
+    },
   },
   plugins: [],
   future: {
