ffede9762af9e97d92fb04c47838d13b9df28e10
Ahhyun <ahhyun@pozalabs.com>
2024-09-25 17:20:07 +0900

    u: Slider export 구문 추가

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ import '../styles/global.css';
 
 export { default as Waveform } from './Waveform/Waveform';
 export type { WaveformHandles } from './Waveform/Waveform';
+export { default as Slider } from './Slider/Slider';
