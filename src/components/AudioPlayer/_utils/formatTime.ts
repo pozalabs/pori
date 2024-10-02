1e96d346fd94a7f28c1d1befba96fddc2af79dda
Ahhyun <ahhyun@pozalabs.com>
2024-10-02 09:30:07 +0900

    c: 타임 포맷에 맞게 문자열로 포맷팅하는 formatTime 유틸 함수 추가

---
+++ src/components/AudioPlayer/_utils/formatTime.ts
@@ -0,0 +1,25 @@
+import type { TimeFormat } from '../_types';
+
+const formatTime = (time: number, format: TimeFormat) => {
+  const hours = Math.floor(time / 3600);
+  const minutes = Math.floor((time % 3600) / 60);
+  const seconds = time % 60;
+
+  const pad = (num: number) => String(num).padStart(2, '0');
+
+  switch (format) {
+    case 'SS':
+    case 'S':
+      return String(time).padStart(format === 'SS' ? 2 : 1, '0');
+    case 'MM:SS':
+      return `${pad(Math.floor(time / 60))}:${pad(seconds)}`;
+    case 'M:S':
+      return `${Math.floor(time / 60)}:${seconds}`;
+    case 'HH:MM:SS':
+      return `${pad(hours)}:${pad(minutes)}:${pad(seconds)}`;
+    case 'H:M:S':
+      return `${hours}:${minutes}:${seconds}`;
+  }
+};
+
+export default formatTime;
