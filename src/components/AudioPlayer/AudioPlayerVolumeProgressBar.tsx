df9b7a67155bc4a5e1ed71fc0970ce6d3b218d81
Ahhyun <ahhyun@pozalabs.com>
2024-10-02 10:31:20 +0900

    c: 볼륨을 보여주는 프로그레스바 추가

---
+++ src/components/AudioPlayer/AudioPlayerVolumeProgressBar.tsx
@@ -0,0 +1,73 @@
+import { useCallback, useContext, useRef } from 'react';
+
+import { AUDIO_PLAYER_PROGRESS_BAR_DEFAULT_VALUE } from './_constants';
+import { AudioPlayerContext } from './AudioPlayerProvider';
+import Slider from '../Slider/Slider';
+
+interface AudioPlayerVolumeProgressBarProps
+  extends Omit<
+    Parameters<typeof Slider>[0],
+    'max' | 'min' | 'value' | 'onChange' | 'onDrag' | 'onDragStart' | 'onDragEnd'
+  > {
+  draggable?: boolean;
+}
+
+const AudioPlayerVolumeProgressBar = ({
+  draggable = AUDIO_PLAYER_PROGRESS_BAR_DEFAULT_VALUE.draggable,
+  step = AUDIO_PLAYER_PROGRESS_BAR_DEFAULT_VALUE.step,
+  ...sliderProps
+}: AudioPlayerVolumeProgressBarProps) => {
+  const { volume, changeVolume } = useContext(AudioPlayerContext);
+
+  const isDraggingRef = useRef(false);
+
+  const onSliderChange = useCallback(
+    (volume: number): void => {
+      changeVolume(volume);
+    },
+    [changeVolume],
+  );
+
+  const onSliderDrag = useCallback(
+    (volume: number): void => {
+      if (!isDraggingRef.current) return;
+
+      onSliderChange(volume);
+    },
+    [onSliderChange],
+  );
+
+  const onSliderDragStart = useCallback(
+    (volume: number): void => {
+      isDraggingRef.current = true;
+      onSliderChange(volume);
+    },
+    [onSliderChange],
+  );
+
+  const onSliderDragEnd = useCallback(
+    (volume: number): void => {
+      if (!isDraggingRef.current) return;
+
+      isDraggingRef.current = false;
+      onSliderChange(volume);
+    },
+    [onSliderChange],
+  );
+
+  return (
+    <Slider
+      {...sliderProps}
+      max={1}
+      min={0}
+      step={step}
+      value={volume}
+      onChange={onSliderChange}
+      onDrag={draggable ? onSliderDrag : undefined}
+      onDragStart={draggable ? onSliderDragStart : undefined}
+      onDragEnd={draggable ? onSliderDragEnd : undefined}
+    />
+  );
+};
+
+export default AudioPlayerVolumeProgressBar;
