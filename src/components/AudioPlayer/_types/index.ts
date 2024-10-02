c27efd37d326dfd304f60a2f0d65ef426b29e65a
Ahhyun <ahhyun@pozalabs.com>
2024-10-02 09:29:38 +0900

    c: 오디오 플레이어에서 사용할 상수 및 타입 추가

---
+++ src/components/AudioPlayer/_types/index.ts
@@ -0,0 +1,28 @@
+import type { Playlist, RepeatModeType } from '../../../hooks/playlist/_types';
+
+export interface AudioPlayerContextDefaultValue {
+  currentSrc: string;
+  currentTime: number;
+  duration: number;
+  isPlaying: boolean;
+  playbackRate: number;
+  playlist: Playlist;
+  repeatMode: RepeatModeType;
+  volume: number;
+  changeCurrentSrc: (currentSrc: string) => void;
+  changeCurrentTime: (currentTime: number) => void;
+  changePlaybackRate: (playbackRate: number) => void;
+  changeRepeatMode: (repeatMode: RepeatModeType) => void;
+  changeVolume: (volume: number) => void;
+  play: () => void;
+  pause: () => void;
+  shiftTimeBackward: () => void;
+  shiftTimeForward: () => void;
+  playNextAudio: (autoplay?: boolean) => void;
+  playPrevAudio: (autoplay?: boolean) => void;
+  stop: () => void;
+  toggleMuted: () => void;
+  togglePlayPause: () => void;
+}
+
+export type TimeFormat = 'SS' | 'S' | 'MM:SS' | 'M:S' | 'HH:MM:SS' | 'H:M:S';
