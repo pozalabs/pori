e1c346d682aa7c18f088efbafb3d5cd6d304e8f1
Ahhyun <ahhyun@pozalabs.com>
2025-01-08 11:37:48 +0900

    u: 유틸 함수 types 폴더 분리

---
+++ src/types/utils/index.ts
@@ -0,0 +1,26 @@
+/**
+ * The parameter type for the fetchAudio function.
+ */
+export interface FetchAudioParams {
+  src: string;
+  chunkSize?: number;
+  limit?: number;
+  retry?: number;
+}
+
+/**
+ * The return type for the fetchAudio function.
+ */
+export interface FetchAudioReturns {
+  url: string;
+  blob: Blob;
+  arrayBuffer: ArrayBuffer;
+}
+
+/**
+ * The return type for the getAudioFileInformation function.
+ */
+export type GetAudioFileInformationReturns = Promise<{
+  audioType: string;
+  audioSize: number;
+}>;
